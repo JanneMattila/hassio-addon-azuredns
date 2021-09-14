716aa6751bd22227d13561c0e127a76d0eb7668d
Janne Mattila <janne.mattila@hotmail.fi>
2021-09-14 16:31:08 +0000

    Use bashio

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bashio
+set -e
+
+bashio::log.info "Azure DNS Zone updater starting..."
 
 # Variables
-AAD_TENANT_ID="<your_tenant_id>"
-AAD_CLIEND_ID="<your_app_id>"
-AAD_CLIEND_SECRET="<your_app_secret>"
+AAD_TENANT_ID=$(bashio::config 'aadTenantId')
+AAD_CLIEND_ID=$(bashio::config 'aadClientId')
+AAD_CLIEND_SECRET=$(bashio::config 'aadClientSecret')
 
-DNS_ZONE_ID="/subscriptions/<your_sub_id>/resourceGroups/<your_rg>/providers/Microsoft.Network/dnszones/<your_dns_zone>"
-RECORD_TYPE="A"
-RECORD_NAME="demo1"
+DNS_ZONE_ID=$(bashio::config 'dnsZoneID')
+RECORD_TYPE=$(bashio::config 'recordType')
+RECORD_NAME=$(bashio::config 'recordName')
 RESOURCE_ID=$DNS_ZONE_ID/$RECORD_TYPE/$RECORD_NAME
 
 ###########################################################
@@ -17,19 +20,19 @@ RESOURCE_ID=$DNS_ZONE_ID/$RECORD_TYPE/$RECORD_NAME
 ###########################################################
 
 export MY_IP=$(curl --no-progress-meter https://api.ipify.org)
-echo "Current public IP: $MY_IP"
+bashio::log.info "Current public IP: $MY_IP"
 
 BODY="client_id=$AAD_CLIEND_ID&client_secret=$AAD_CLIEND_SECRET&scope=https://management.azure.com/.default&grant_type=client_credentials"
 ACCESS_TOKEN=$(curl --no-progress-meter -H "Content-Type: application/x-www-form-urlencoded" --data $BODY "https://login.microsoftonline.com/$AAD_TENANT_ID/oauth2/v2.0/token" | jq -r .access_token)
 CURRENT_IP=$(curl --no-progress-meter -H "Authorization: Bearer $ACCESS_TOKEN" "https://management.azure.com$RESOURCE_ID?api-version=2018-05-01" | jq -r .properties.ARecords[0].ipv4Address)
-echo "Current IP in DNS Zone: $CURRENT_IP"
+bashio::log.info "Current IP in DNS Zone: $CURRENT_IP"
 
 if test "$CURRENT_IP" = "$MY_IP"; then
-    echo "IP address match, so no need to update DNS Zone"
+    bashio::log.info "IP address match, so no need to update DNS Zone"
 else
-    echo "DNS Zone update is required due to updated public IP address"
+    bashio::log.info "DNS Zone update is required due to updated public IP address"
     # https://docs.microsoft.com/en-us/rest/api/dns/record-sets/update#patch-a-recordset
     PATCH_BODY="{ \"properties\": { \"ARecords\": [ { \"ipv4Address\": \"$MY_IP\" } ] } }"
     STATUS=$(curl -X PATCH --no-progress-meter -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" --data "$PATCH_BODY" "https://management.azure.com$RESOURCE_ID?api-version=2018-05-01" | jq -r .properties.provisioningState)
-    echo "IP Update state: $STATUS"
+    bashio::log.info "IP Update state: $STATUS"
 fi
